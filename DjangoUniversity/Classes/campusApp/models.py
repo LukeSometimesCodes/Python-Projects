ec7b823b6084cd617d7ec5073c7e188de2622edc
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-03-06 17:06:37 -0700

    django uni

--- DjangoUniversity/Classes/campusApp/models.py
+++ DjangoUniversity/Classes/campusApp/models.py
@@ -13,6 +13,7 @@ class UniversityCampus(models.Model):
 
     # Displays the object output values in the form of a string
     def __str__(self):
+        return self.title
         # Returns the input value of the title and instructor name
         # field as the tuple to display in the browser instead of the default titles
         display_course = '{0.title}: {0.instructor_name}'
--- DjangoUniversity/Classes/classApp/models.py
+++ DjangoUniversity/Classes/classApp/models.py
@@ -6,7 +6,6 @@ from django.db.models import IntegerField
 
 class UniversityClasses(models.Model):
     title = models.CharField(max_length=60, default='', blank=True, null=False)
-    course_number = models.IntegerField(default='', blank=True, null=False)
     instructor_name = models.CharField(max_length=60, default='', blank=True, null=False)
     duration = models.FloatField(null=True, blank=True, default=None)
 
@@ -15,6 +14,7 @@ class UniversityClasses(models.Model):
 
     # Displays the object output values in the form of a string
     def __str__(self):
+        return self.title
         # Returns the input value of the title and instructor name
         # field as the tuple to display in the browser instead of the default titles
         display_course = '{0.title}: {0.instructor_name}'
